23a0a5de81c0594e762e93b9d263095715c1080a
Jav <j.a.cabrerapuente@gmail.com>
2020-02-08 10:36:51 -0600

    cover page and first steps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoverComponent } from './cover/cover.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CoverComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [ AppComponent,
+                                   CoverComponent ];
